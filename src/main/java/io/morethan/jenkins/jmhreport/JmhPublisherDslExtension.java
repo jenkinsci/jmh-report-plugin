dba8c51d82f0fb2a27fff98c6076f970c117b54c
Maik Fröbe <maik44froebe@web.de>
2018-09-30 01:32:27 +0200

    Add jmh-dsl-methods

---
+++ src/main/java/io/morethan/jenkins/jmhreport/JmhPublisherDslExtension.java
@@ -0,0 +1,16 @@
+package io.morethan.jenkins.jmhreport;
+
+import hudson.Extension;
+import javaposse.jobdsl.dsl.helpers.publisher.PublisherContext;
+import javaposse.jobdsl.plugin.ContextExtensionPoint;
+import javaposse.jobdsl.plugin.DslExtensionMethod;
+
+@Extension(optional=true)
+public class JmhPublisherDslExtension extends ContextExtensionPoint
+{
+	@DslExtensionMethod(context=PublisherContext.class)
+	public Object jmhPublisher(String resultPath)
+	{
+		return new RunPublisher(resultPath);
+	}
+}
